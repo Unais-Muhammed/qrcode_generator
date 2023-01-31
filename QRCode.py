08e1394d1a637c73c7981e188e8bfaa1a4126115
Unais-Muhammed <unaismuhammed001@gmail.com>
2023-01-31 06:21:07 +0530

    first commit in qrcode_generator

---
+++ QRCode.py
@@ -0,0 +1,17 @@
+import qrcode
+
+data = input("Enter data to be encoded in QR code: ")
+
+qr = qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=5
+)
+
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qr.png")
+
+print("QR code saved as qr.png")
